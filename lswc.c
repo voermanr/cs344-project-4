d36edc71f767aa844f50216c6cad033e8eede8e9
Ross Voerman <voermanr@oregonstate.edu>
2023-02-01 16:40:46 -0800

    error handling and refactoring

--- lswc.c
+++ lswc.c
@@ -3,10 +3,9 @@
 #include "unistd.h"
 #include "string.h"
 
-int tokenize_string(char *buf, char **argv);
+int execute_child(const int *tube, char **fnc);
 
-int build_argv(int *argc, char *buf, char **argv);
-
+int execute_parent(const int *tube, char **fnc, const int *argc, char *argv[]);
 int main(int argc, char *argv[]) {
 
     if (argc > 2) {
@@ -14,25 +13,50 @@ int main(int argc, char *argv[]) {
     }
 
     int tube[2];
-    pipe(tube);
-
-    if (argc == 1) {
-
+    if (pipe(tube) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
 
     pid_t pid = fork();
-    char *fnc[2];
-    if (pid == 0) {
-        dup2(STDIN_FILENO, tube[0]);
-        close(tube[1]);
-        fnc[0] = "wc";
-        fnc[1] = "-l";
-        execlp(fnc[0],fnc[1]);
-    } else {
-        dup2(tube[1], STDOUT_FILENO);
-        close(tube[0]);
-        fnc[0] = "ls";
-        fnc[1] = "-1a";
-        execlp(fnc[0], fnc[1]);
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
+
+    char *fnc[3];
+
+    pid == 0 ? execute_child(tube, fnc) : execute_parent(tube, fnc, &argc, argv);
 }
+
+int execute_parent(const int *tube, char **fnc, const int *argc, char *argv[]) {
+    close(tube[0]);
+    dup2(tube[1], STDOUT_FILENO);
+    close(tube[1]);
+
+    fnc[0] = "ls";
+    fnc[1] = "-1a";
+    char *dir = ".";
+
+    if (*argc == 2) {
+        dir = argv[1];
+    }
+
+    execlp(fnc[0], fnc[0], fnc[1], dir, NULL);
+
+    return 0;
+}
+
+int execute_child(const int *tube, char **fnc) {
+    close(tube[1]);
+    dup2(tube[0], STDIN_FILENO);
+    close(tube[0]);
+
+    fnc[0] = "wc";
+    fnc[1] = "-l";
+
+    execlp(fnc[0],fnc[0], fnc[1], NULL);
+
+    return 0;
+}
+
