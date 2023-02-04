3fe73cfd4e63bd1de118c7bdc88e31680f59d8d6
Ross Voerman <voermanr@oregonstate.edu>
2023-02-03 17:23:10 -0800

    making progress

--- redirout.c
+++ redirout.c
@@ -3,10 +3,52 @@
 #include "unistd.h"
 #include "fcntl.h"
 
-int main (int argc, char **argv){
-    //TODO: Check command line
+#define BUFF_SIZE 1024
+
+int main (int argc, char *argv[]){
+    if (argc < 2) {
+        fprintf(stderr,"too few arguments.");
+        return EXIT_FAILURE;
+    }
 
     int fd = open(argv[1], O_CREAT | O_WRONLY, 644);
 
+    int tube[2];
+    if (pipe(tube) == -1) {
+        perror("pipe");
+        return EXIT_FAILURE;
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+
+    if (pid == 0) {
+        close(tube[0]);
+        dup2(tube[1], STDOUT_FILENO);
+        close(tube[1]);
+        execvp(argv[2], argv + 2);
+        perror("execvp");
+        return EXIT_FAILURE;
+    }
+
+    else {
+        char buf[BUFF_SIZE];
+        close(tube[1]);
+
+        ssize_t bytes_read, bytes_wrote;
+
+        while ((bytes_read = read(tube[0],buf, sizeof(buf))) > 0) {
+            write(fd, buf,bytes_read);
+        }
+        if (bytes_read == -1) {
+            perror("read");
+        }
+        if (bytes_wrote == -1) {
+            perror("write");
+        }
+    }
     return 0;
 }
